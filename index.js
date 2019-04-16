549f387da4c571e4ba38d5bd9f6e6a75b6f42c36
Alexander gribenchenko <samnite87@gmail.com>
2019-04-16 15:17:48 +0300

    increment html, css, date controller

--- index.js
+++ index.js
@@ -14,20 +14,25 @@ class Client {
     }
 }
 
-
+document.querySelector('.loader').style.display = 'none';
 const getData = async () => {
+    document.querySelector('.loader').style.display = 'block';
+    document.querySelector('.search_results').innerHTML = '';
     try {
         const result = await fetch(`https://script.googleusercontent.com/macros/echo?user_content_key=adAPGf6PEivqPpn1IiOGQ4ReXlmr6SFVdriSCmB8bhHs60eEuHJW75MFpf3cqyTiOSpAQOuDYvq1l5auwJgNAgeZF-91yaRvm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBOBjU-9TSGvuOhPqIMTJQKhc7LEJ9fQ02J1-OwLiR8vDgGkFwqf1OH7jbd34yzKwhdIug5zkO6q&lib=MsoO2fZ7GpcAvxSnFqDRWHzPTuxB3ZxFA`);
-        const data = await result.json();
-
+        const data = await result.json();        
         userID = document.querySelector('.search_input').value;
+
         data.result.forEach(e => {
             results.push(new Client(e[0], e[2], e[3], e[4], e[5], e[6], e[7], e[10], e[11], e[12]));
-        }); 
+        });
+         
         renderSearch(userID);
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('.search_input').value = '';
 
     } catch(error) {
-        alert(`Такого ID не сущетсвует. ID: ${userID}`);
+        alert(`Такого ID не сущетсвует.`);
     }
 };
 
@@ -39,17 +44,23 @@ let userID;
 
 const renderSearch = param => {
     const currentResult = results.find(cur => cur.id == param);
+    
     document.querySelector('.search_results').innerHTML = `
-        <ul>
-            <li>Номер заказа (id): ${currentResult.id}</li>
-            <li>Клиент: ${currentResult.name}</li>
-            <li>Принят в сервис: ${currentResult.dateIn}</li>
-            <li>Тип услуги: ${currentResult.type}</li>
-            <li>Модель техники: ${currentResult.model}</li>
-            <li>Описание неисправности: ${currentResult.descr}</li>
-            <li>Выполненные работы: ${currentResult.work}</li>
-            <li>Дата выдачи: ${currentResult.dateOut}</li>
-            <li>Статус: ${currentResult.status}</li>
+        <ul class="border">
+            <li><span>Номер заказа:</span> <em>${currentResult.id}</em></li>
+            <li><span>Принят в сервис:</span> <em>${formatDate(currentResult.dateIn)}</em></li>
+            <li><span>Тип услуги:</span> <em>${currentResult.type}</em></li>
+            <li><span>Модель техники:</span> <em>${currentResult.model}</em></li>
+            <li><span>Описание неисправности:</span> <em>${currentResult.descr}</em></li>
+            <li><span>Выполненные работы:</span> <em>${currentResult.work}</em></li>
+            <li><span>Дата выдачи:</span> <em>${formatDate(currentResult.dateOut)}</em></li>
+            <li><span>Статус:</span> <em>${currentResult.status}</em></li>
         </ul>
     `;
+};
+
+const formatDate = day => {
+    const date = new Date(day); 
+    if(day) return `${date.getDate()} ${date.toLocaleString('ru', {month: 'long'})} ${date.getFullYear()}`; 
+    else return `неверная дата`;    
 };
